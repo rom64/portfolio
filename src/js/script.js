24393cc85d017289a949ef677d23a9ec09dbb02a
Roman <litvinroman2009@gmail.com>
2021-02-21 00:19:36 +0200

    add js

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,19 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeBtn = document.querySelector('.menu__close'),
-    li= document.querySelector('.menu__link');
+    lis= document.querySelectorAll('.menu__link');
 
-hamburger.addEventListener('click', ()=>{
-    menu.classList.add('active');
-});
-closeBtn.addEventListener('click', ()=>{
-    menu.classList.remove('active');
-});
+    hamburger.addEventListener('click', ()=>{
+        menu.classList.add('active');
+    });
+    closeBtn.addEventListener('click', ()=>{
+        menu.classList.remove('active');
+    });
+    const fadeMenu=() => {
+        menu.classList.remove('active');
+    };
 
-li.addEventListener('click', function close(){
-    menu.classList.remove('active');
 
-    setInterval(close, 2000);
-});
+    lis.forEach(item =>{
+        item.addEventListener('click', fadeMenu);
+    });
