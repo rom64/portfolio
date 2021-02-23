aa6959f5f4db3f4ac45183abc1f6254d74e897c9
Roman <litvinroman2009@gmail.com>
2021-02-23 15:24:08 +0200

    add localisation +fix bugs

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
     closeBtn = document.querySelector('.menu__close'),
     lis= document.querySelectorAll('.menu__link');
+let timerId ;
 
     hamburger.addEventListener('click', ()=>{
         menu.classList.add('active');
@@ -15,5 +16,42 @@ const hamburger = document.querySelector('.hamburger'),
 
 
     lis.forEach(item =>{
-        item.addEventListener('click', fadeMenu);
+        item.addEventListener('click', ()=>{
+            timerId=setTimeout(fadeMenu, 1000);
+        });
     });
+
+const localisation = document.querySelector('.localisation'),
+    item= document.querySelector('.localisation__item');
+    localisation.addEventListener('click', ()=>{
+        item.classList.toggle('active');
+    });
+
+$(function(){
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: 'smart.php',
+            data:$(this).serialize()
+        }).done(function (){
+            $(this).find('input').val('');
+
+            $('form').trigger('reset');
+        });
+        return  false;
+    });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            $('.totop').fadeIn();
+        } else {
+            $('.totop').fadeOut();
+        }
+    });
+   $('.totop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+});
